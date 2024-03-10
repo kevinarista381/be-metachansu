861653b3e6807e9a462b2970c2ff5f161f9bb764
Kevin Eontrainer <contactkevinarista@gmail.com>
2024-03-10 23:41:16 +0700

    added authRouter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,7 +1,9 @@
+const login = require("../controllers/Auth/login");
+const multer = require("multer");
+const upload = multer();
+
 require("dotenv").config();
 
 module.exports = (app) => {
-  app.post("/login", (req, res) => {
-    let { userName, password } = req.body;
-  });
+  app.post("/login", upload.none(), (req, res) => login(req, res));
 };
