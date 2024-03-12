0b76f7b0e52040e10065881618b272d180d7f918
Kevin Eontrainer <contactkevinarista@gmail.com>
2024-03-12 23:53:18 +0700

    created jwt token upon logging in

--- controllers/Auth/login.js
+++ controllers/Auth/login.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const { User } = require("../../models");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res) => {
   let { userName, password } = req.body;
@@ -10,9 +12,12 @@ module.exports = async (req, res) => {
 
     bcrypt.compare(password, user.dataValues.password, (err, result) => {
       if (err) return res.sendStatus(500);
-      if (result) {
-        return res.sendStatus(200);
-      }
+      if (!result) return res.sendStatus(404);
+      let accessToken = jwt.sign(
+        user.dataValues,
+        process.env.ACCESS_TOKEN_SECRET
+      );
+      return res.json({ user, accessToken });
     });
   } catch (err) {
     return res.status(500).send(err.message);
